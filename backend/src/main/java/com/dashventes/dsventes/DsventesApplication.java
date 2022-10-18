7c661bbf9ea1f0ce1b919000d81787eed390a320
Thales Santana <generationt2h@gmail.com>
2022-10-18 00:10:47 -0300

    Criacao do Projeto / creation du projet

---
+++ backend/src/main/java/com/dashventes/dsventes/DsventesApplication.java
@@ -0,0 +1,13 @@
+package com.dashventes.dsventes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsventesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsventesApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/dashventes/dsventes/DsventesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dashventes.dsventes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsventesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

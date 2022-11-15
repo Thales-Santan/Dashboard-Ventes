9c709e77f9cf23bd3adde1d7da33337b284ec82b
Thales Santana <generationt2h@gmail.com>
2022-11-14 21:58:08 -0300

    Basique statique éléments

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App développé par <a href="https://github.com/Thales-Santan?tab=repositories" target="_blank" rel="noreferrer">Thales Santana</a></p>
+                <p className="text-light"><small><strong>Semaine Spring React</strong><br />
+                    Événement organisé par l’école DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;

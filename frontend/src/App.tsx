48a1f92d90f4f85e78e58331058d0631aa3d3fcf
Thales Santana <generationt2h@gmail.com>
2022-12-25 22:25:05 -0300

    Graphiques statiques

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,31 @@
 import NavBar from "components/NavBar";
 import Footer from "components/Footer";
 import DataTable from "components/DataTable";
+import BarChart from "components/BarChart";
+import DonutChart from "components/DonutChart";
 
 function App() {
   return (
     <>
       <NavBar />
       <div className="container">
+        <h1 className="text-primary py-3">DashBoard des Ventes</h1>
+
+      <div className="row px-3">
+        <div className="col-sm-6">
+          <h5 className="text-center text-secondary">Taux de Succès (%)</h5>
+          <BarChart/>
+        </div>
+        <div className="col-sm-6">
+          <h5 className="text-center text-secondary">Toutes Les Ventes</h5>
+          <DonutChart/>
+        </div>
+      </div>
+
+      <div className="py-3">
+        <h2 className="text-primary">Toutes Les Ventes</h2>
+      </div>
+
         <DataTable />
       </div>
       <Footer />
